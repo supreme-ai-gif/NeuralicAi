d03fd4484eb83ec4fe1bc1832b42273697c2c152
supreme-ai-gif <obuegenesis9@gmail.com>
2025-11-16 20:58:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Allow all origins
-    allow_credentials=True, you 
+    allow_credentials=True, 
     allow_methods=["*"],
     allow_headers=["*"],
 )
